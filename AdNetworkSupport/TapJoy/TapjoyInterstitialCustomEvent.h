ec7ff079b5e46a0645158ec74eb20a22c5722998
Matt Cai <mcai@tunein.com>
2016-11-29 17:49:35 -0800

    Mopub error report

---
+++ AdNetworkSupport/TapJoy/TapjoyInterstitialCustomEvent.h
@@ -0,0 +1,13 @@
+
+#if __has_include(<MoPub/MoPub.h>)
+    #import <MoPub/MoPub.h>
+#else
+    #import "MPInterstitialCustomEvent.h"
+#endif
+
+/*
+ * Certified with version 11.5.1 of the Tapjoy SDK.
+ */
+
+@interface TapjoyInterstitialCustomEvent : MPInterstitialCustomEvent
+@end
---
+++ AdNetworkSupport/TapJoy/TapjoyRewardedVideoCustomEvent.h
@@ -0,0 +1,14 @@
+
+#if __has_include(<MoPub/MoPub.h>)
+    #import <MoPub/MoPub.h>
+#else
+    #import "MPRewardedVideoCustomEvent.h"
+#endif
+
+/*
+ * Certified with version 11.5.1 of the Tapjoy SDK.
+ */
+
+@interface TapjoyRewardedVideoCustomEvent : MPRewardedVideoCustomEvent
+
+@end
